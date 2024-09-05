afa99a7d83b1cb3ad6822599f195ae820197255a
Achyut <onestring22@gmail.com>
2024-09-05 18:36:15 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import { configDotenv } from 'dotenv'
 import dbConnect from './db/index.js'
+import app from './app.js'
 
 
 configDotenv('../env')
 dbConnect( )
+.then((value)=>{
 
+ app.on('error', (error)=>{
+    console.log(`Error on :: ${error}`)
+    throw error
+ })
+
+ app.listen(process.env.PORT || 8000, ()=>{
+    console.log(`⁜ Server is running at port ${process.env.PORT ? process.env.PORT : 8000}`)
+ })
+}
+
+)
+.catch((error)=>{
+    console.log('MONGODB Connection failed ::', error)
+})
